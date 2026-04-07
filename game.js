f788b3109684fee618e615ba2465ef58c6f7e70a
dev-salih <salihmu79@gmail.com>
2026-04-07 19:00:19 +0300

    first challenge completed

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
-var gamePattern = []; 
+// 3
 var buttonColours = ["red", "blue", "green", "yellow"];
+// 5
+var gamePattern = []; 
 var randomChosenColour = buttonColours.randomNumber;
-
+//1
 function nextSequence(){
+    // 2
     var randomNumber = Math.floor(Math.random() * 4);
+    // 4
+    var randomChosenColour = buttonColours[randomNumber];
+    // 6
+    gamePattern.Push(randomChosenColour);
 }
