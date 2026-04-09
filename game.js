7d37f8d6ba75f678a7a653789a98703e3d10af5b
dev-salih <salihmu79@gmail.com>
2026-04-09 09:46:41 +0300

    playSound function added

--- game.js
+++ game.js
@@ -1,28 +1,28 @@
-// 3
-var buttonColours = ["red", "blue", "green", "yellow"];
-// 5
-var gamePattern = []; 
+
+var buttonColours = ["red", "blue", "green", "yellow"];// 3
+var gamePattern = []; // 5
 
 var userClickedPattern = [];
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+
+    playSound(userChosenColour);
 })
 
-//1
-function nextSequence(){
-    // 2
-    var randomNumber = Math.floor(Math.random() * 4);
-    // 4
-    var randomChosenColour = buttonColours[randomNumber];
-    // 6
-    gamePattern.Push(randomChosenColour);
+
+function nextSequence(){   //1
+    var randomNumber = Math.floor(Math.random() * 4); // 2
+    var randomChosenColour=buttonColours[randomNumber];// 4
+    gamePattern.Push(randomChosenColour);  // 6
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
-    audio.play();
+    playSound(randomChosenColour);
 };
 
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play();
+}
 
