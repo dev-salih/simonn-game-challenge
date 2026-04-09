84bfd01316cbb4b7d770542515d1ce87fb742d86
dev-salih <salihmu79@gmail.com>
2026-04-09 11:25:43 +0300

    keypress method used

--- game.js
+++ game.js
@@ -1,36 +1,53 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];// 3
+
 var gamePattern = []; // 5
-
 var userClickedPattern = [];
-$(".btn").click(function(){
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
 
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-})
+var started = false;
+var level = 0;
+$(document).keypress(function (){
+  if (!started) {
+    $("#level-title").text("level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+$(".btn").click(function(){
+  
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
 
 
 function nextSequence(){   //1
-    var randomNumber = Math.floor(Math.random() * 4); // 2
-    var randomChosenColour=buttonColours[randomNumber];// 4
-    gamePattern.Push(randomChosenColour);  // 6
+  level++;
+  $("#level-title").text("level " + level);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  var randomNumber = Math.floor(Math.random() * 4); // 2
+  var randomChosenColour=buttonColours[randomNumber];// 4
+  gamePattern.Push(randomChosenColour);  // 6
 
-    playSound(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 };
 
 function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3")
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3")
+  audio.play();
 }
 
 function animatePress(currentColour){
-    $("#" + currentColour).addClass("pressed");
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+  $("#" + currentColour).removeClass("pressed");
+  }, 100);    
+};
 
-    setTimeout(function() {
-    $("#" + currentColour).removeClass("pressed");
-    }, 100);    
-}
+
+
